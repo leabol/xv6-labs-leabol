81dfba93a6ca14df837a31294d513f33d96028b7
Leabol <1791722441@qq.com>
2026-04-06 16:01:56 +0800

    inti lab env

---
+++ .vscode/lab_intellisense_defines.h
@@ -0,0 +1,6 @@
+/* Auto-generated from conf/lab.mk. Do not edit manually. */
+#ifndef XV6_LAB_INTELLISENSE_DEFINES_H
+#define XV6_LAB_INTELLISENSE_DEFINES_H
+#define LAB_THREAD 1
+#define SOL_THREAD 1
+#endif
