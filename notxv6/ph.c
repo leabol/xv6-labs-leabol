b087e335f1e4daa0f345bc5f2e631c7b66428f1e
Leabol <1791722441@qq.com>
2026-04-07 12:18:33 +0800

    Add mutex locking to put function for thread safety

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,7 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t lock;
 
 double
 now()
@@ -52,7 +53,9 @@ void put(int key, int value)
     e->value = value;
   } else {
     // the new is new.
+    pthread_mutex_lock(&lock);
     insert(key, value, &table[i], table[i]);
+    pthread_mutex_unlock(&lock);
   }
 
 }
@@ -121,6 +124,7 @@ main(int argc, char *argv[])
   //
   // first the puts
   //
+  pthread_mutex_init(&lock,NULL);
   t0 = now();
   for(int i = 0; i < nthread; i++) {
     assert(pthread_create(&tha[i], NULL, put_thread, (void *) (long) i) == 0);
