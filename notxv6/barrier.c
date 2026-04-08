85e1acb15023233794901529587012aeaf0e452e
Leabol <1791722441@qq.com>
2026-04-08 20:52:37 +0800

    Implement barrier synchronization using mutex and condition variables

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,20 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  int my_round = bstate.round;
+  bstate.nthread++;
+
+  if (nthread == bstate.nthread){
+    bstate.nthread = 0;
+    bstate.round++;
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }else {
+    while(bstate.round == my_round){
+      pthread_cond_wait(&bstate.barrier_cond,&bstate.barrier_mutex);
+    }
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
