ee50187a8ab25fb5d2adc1f47a4cff82881eed6f
Leabol <1791722441@qq.com>
2026-04-14 19:20:55 +0800

    init lab env

---
+++ .vscode/lab_intellisense_defines.h
@@ -0,0 +1,6 @@
+/* Auto-generated from conf/lab.mk. Do not edit manually. */
+#ifndef XV6_LAB_INTELLISENSE_DEFINES_H
+#define XV6_LAB_INTELLISENSE_DEFINES_H
+#define LAB_FS 1
+#define SOL_FS 1
+#endif
